b84cfae5bb0a6beebe5e11e40cea6b9edbd69c71
phanvantri <36974573+phanvantri@users.noreply.github.com>
2019-03-05 22:28:33 +0700

    Update  xong giao diện với làm gần xong các phím số...Haizz

--- app/src/main/java/com/example/calculator/ConActivity.java
+++ app/src/main/java/com/example/calculator/ConActivity.java
@@ -1,13 +1,34 @@
 package com.example.calculator;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ConActivity extends AppCompatActivity {
 
+    //Khởi tạo 2 biến tvMath, tvResult
+    private  String tvMath;
+    private  String tvResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_con);
+        Intent intent = this.getIntent();
+        this.tvMath=intent.getStringExtra("tvMath");
+        this.tvResult=intent.getStringExtra("tvResult");
+        TextView txtMath=(TextView) this.findViewById(R.id.tvMath);
+        txtMath.setText(tvMath);
+        TextView txtResult=(TextView)this.findViewById(R.id.tvResult);
+        txtResult.setText(tvResult);
+
+    }
+    // Quay về giao diện ban đầu.
+    public void moveLeftClick(View view)  {
+        // Calling onBackPressed().
+        // Gọi phương thức onBackPressed().
+        this.onBackPressed();
     }
 }
--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -5,14 +5,23 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements  View.OnClickListener{
 
     private TextView textMath;
     private TextView textResult;
+    private  int[] idButton={
+            R.id.btnZero,R.id.btnOne,R.id.btnTwo,R.id.btnThree,R.id.btnFour,
+            R.id.btnFive,R.id.btnSix,R.id.btnSeven,R.id.btnEight,R.id.btnNine,
+            R.id.btnAdd,R.id.btnSub,R.id.btnMul,R.id.btnDiv,
+            R.id.btnPlus
+           ,R.id.btnMod,R.id.btnSpace,
+            R.id.btnResult,R.id.btnReset, R.id.btnDelete
+    };
     public static final int MY_REQUEST_CODE = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +29,68 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         this.textMath=(TextView)this.findViewById(R.id.tvMath);
         this.textResult=(TextView)this.findViewById(R.id.tvResult);
+        connectView();
+
     }
 
+
+    //Bắt sự kiện cho từng Button
+    private void connectView() {
+        textMath = (TextView) findViewById(R.id.tvMath);
+        textResult = (TextView) findViewById(R.id.tvResult);
+
+        for (int i = 0; i < idButton.length; i++) {
+            findViewById(idButton[i]).setOnClickListener(this);
+        }
+    }
+    private void init() {
+        textMath.setText("|");
+        textResult.setText("0");
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        String text="";
+        // check button number and button operator
+        for (int i = 0; i < idButton.length - 3; i++) {
+            if (id == idButton[i]) {
+                 text = ((Button) findViewById(id)).getText().toString();
+                // clear char | on top
+                if (textMath.getText().toString().trim().equals("|")) {
+                    textResult.setText(" ");
+                }
+                textMath.append(text+" ");
+                return;
+            }
+        }
+        if(id==R.id.btnDelete){
+            init();
+            return;
+        }
+
+        // clear screen
+        if (id == R.id.btnReset) {
+            init();
+            return;
+        }
+
+        // calculation
+        if (id == R.id.btnResult) {
+           // cal();
+        }
+
+    }
+
+
+
+
+
+
+
     // The method is called when the user clicks on "Show Greeting" button.
-    public void MoveClick(View view)  {
+    public void moveRightClick(View view)  {
 
         String textMath= this.textMath.getText().toString();
         String textResult= this.textResult.getText().toString();
@@ -37,4 +104,6 @@ public class MainActivity extends AppCompatActivity {
         // Start Activity and get feedback.
         this.startActivityForResult(intent, MY_REQUEST_CODE);
     }
+
+
 }
